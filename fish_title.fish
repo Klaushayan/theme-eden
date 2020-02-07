86e1b9e91a554dd3900dc484b1e25147bb1ba410
klaushayan <klaus_hybrid33@yahoo.com>
2020-02-07 18:26:56 +0330

    title bar of the terminal is now more expressive

--- fish_title.fish
+++ fish_title.fish
@@ -1,3 +1,11 @@
 function fish_title
 # Customize the title bar of the terminal window.
+    if test  $_ = fish
+        echo (string replace $HOME '~' $PWD)
+    else
+        echo $argv (set -q SSH_CONNECTION; and hostname)":" 
+    end
 end
+
+
+
